47a13294c1cbea5656c32ae95717cfc394b2e33f
Mohammad Shabeer <moshdev2213@gmail.com>
2025-03-14 23:36:16 +0530

    fix:ersHandler

--- src/utils/ResponseHandler/ResponseHandler.js
+++ src/utils/ResponseHandler/ResponseHandler.js
@@ -2,7 +2,7 @@ import HttpType from "./HttpType.js";
 
 const response = (res, code, data) => {
     const status = HttpType.getStatus(code);
-    const success = code >= 200 && code < 300; // Success for 2xx status codes
+    const success = code >= 200 && code < 300 && !data?.error; // Success for 2xx status codes, and no error message
 
     const payLoad = {
         code,
